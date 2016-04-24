aecdcd223138a2974ef81d2c645124b3f48994d2
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2016-04-24 18:49:57 +0200

    Added release script

---
+++ makerelease.ps1
@@ -0,0 +1,22 @@
+cd 'Visual Studio Project Template C#'
+& 'C:\Program Files\7-Zip\7z.exe' a -tzip NppPlugin.zip *
+cd ..
+& 'C:\Program Files\7-Zip\7z.exe' a -tzip c:\temp\nppDemoAndProjectTemplate.zip *
+cd 'Visual Studio Project Template C#'
+rm NppPlugin.zip
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
