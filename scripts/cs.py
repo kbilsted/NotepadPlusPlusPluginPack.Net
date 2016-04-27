7628ef3b34870825c55debc2ed31f3ecfab37e57
dail8859 <dail8859@yahoo.com>
2016-04-27 19:48:02 -0400

    Add Python scripts from Scintilla

---
+++ scripts/cs.py
@@ -0,0 +1,32 @@
+import Face
+from FileGenerator import Regenerate
+
+def printLexCSFile(f):
+	out = []
+	for name in f.order:
+		v = f.features[name]
+		if v["FeatureType"] in ["fun", "get", "set"]:
+			if "Comment" in v: out.extend(["        /// " + line for line in v["Comment"]])
+			featureDefineName = "SCI_" + name.upper()
+			out.append("        " + featureDefineName + " = " + v["Value"] + ",")
+			out.append("")
+		elif v["FeatureType"] in ["evt"]:
+			featureDefineName = "SCN_" + name.upper()
+			if "Comment" in v: out.extend(["        /// " + line for line in v["Comment"]])
+			out.append("        " + featureDefineName + " = " + v["Value"] + ",")
+			out.append("")
+		elif v["FeatureType"] in ["val"]:
+			if not ("SCE_" in name or "SCLEX_" in name):
+				if "Comment" in v: out.extend(["        /// " + line for line in v["Comment"]])
+				out.append("        " + name + " = " + v["Value"] + ",")
+				out.append("")
+	out[-1] = out[-1][:-1] # drop the very last comma of the last line
+	return out
+
+def main():
+	f = Face.Face()
+	f.ReadFromFile("Scintilla.iface")
+	Regenerate("../Visual Studio Project Template C#/Integration/Scintilla_iface.cs", "/* ", printLexCSFile(f))
+
+if __name__ == "__main__":
+	main()
