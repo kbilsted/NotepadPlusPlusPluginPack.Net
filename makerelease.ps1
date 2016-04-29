ff7d37b69172a645815e47a3bdf6f5a92d602df4
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2016-04-29 15:01:20 +0200

    Improved release script

--- makerelease.ps1
+++ makerelease.ps1
@@ -2,8 +2,7 @@ cd 'Visual Studio Project Template C#'
 & 'C:\Program Files\7-Zip\7z.exe' a -tzip NppPlugin.zip *
 cd ..
 & 'C:\Program Files\7-Zip\7z.exe' a -tzip c:\temp\nppDemoAndProjectTemplate.zip *
-cd 'Visual Studio Project Template C#'
-rm NppPlugin.zip
+rm 'Visual Studio Project Template C#\NppPlugin.zip'
 
 
 
