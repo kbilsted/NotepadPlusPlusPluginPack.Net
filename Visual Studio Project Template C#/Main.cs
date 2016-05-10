6e449fa8a03f6466d0b5e027a0416bd47c9bbd64
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2016-05-10 21:29:37 +0200

    fix building errors in plugin infrastructure

--- Visual Studio Project Template C#/Main.cs
+++ Visual Studio Project Template C#/Main.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
-using NppPluginNET;
+using Kbg.NppPluginNET;
 
 namespace $safeprojectname$
 {
