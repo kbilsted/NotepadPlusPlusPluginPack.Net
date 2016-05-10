bfedaca3772d53eb77ece78f5530b6e62c2e76a5
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2016-05-10 21:29:19 +0200

    Make buildscript also copy pluginbase to VS folder

--- makerelease.ps1
+++ makerelease.ps1
@@ -1,5 +1,7 @@
 cd 'Visual Studio Project Template C#'
 & 'C:\Program Files\7-Zip\7z.exe' a -tzip NppPlugin.zip *
+$path = [Environment]::GetFolderPath("MyDocuments")+'\Visual Studio 2015\Templates\ProjectTemplates\Visual C#\'
+copy NppPlugin.zip $path
 cd ..
 & 'C:\Program Files\7-Zip\7z.exe' a -tzip c:\temp\nppDemoAndProjectTemplate.zip *
 rm 'Visual Studio Project Template C#\NppPlugin.zip'
