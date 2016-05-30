0c48619272d825f8e5ad0329dea3565e803b9772
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2016-05-30 21:48:30 +0200

    documentation

--- Visual Studio Project Template C#/Main.cs
+++ Visual Studio Project Template C#/Main.cs
@@ -23,6 +23,13 @@ namespace Kbg.NppPluginNET
         public static void OnNotification(ScNotification notification)
         {  
             // This method is invoked whenever something is happening in notepad++
+            // use eg. as
+            // if (notification.Header.Code == (uint)NppMsg.NPPN_xxx)
+            // { ... }
+            // or
+            //
+            // if (notification.Header.Code == (uint)SciMsg.SCNxxx)
+            // { ... }
         }
 
         internal static void CommandMenuInit()
