0eddd88242c02c75276dda657b88a8a21bd48269
Savas Ziplies <savas.ziplies@project-engineers.de>
2017-02-21 12:49:20 +0100

    Changed: Updated to newest plugin version

--- Visual Studio Project Template C#/PluginInfrastructure/IScintillaGateway.cs
+++ Visual Studio Project Template C#/PluginInfrastructure/IScintillaGateway.cs
@@ -1,5 +1,4 @@
-﻿// NPP plugin platform for .Net v0.93.87 by Kasper B. Graversen etc.
-
+﻿// NPP plugin platform for .Net v0.93.96 by Kasper B. Graversen etc.
 using System;
 using static Kbg.NppPluginNET.PluginInfrastructure.Win32;
 
